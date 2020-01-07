807b3d22217d0ddbf47845bb8a759dc42f4fbe7a
邓健 <jdeng@senparc.com>
2020-01-07 15:59:39 +0800

    merge code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lottie-demo
+#### Lottie 是 Airbnb 开源的一个支持 Web、Android、iOS 以及 ReactNative等平台的动画库，它可以结合 AE 和 Bodymovin 来快速实现跨平台动画。
+# run
+1. `npm install`
+2. `npm run serve`
+
+##### AE模板库：https://lottiefiles.com/
--- vue-lottie/src/App.vue
+++ vue-lottie/src/App.vue
@@ -1,19 +1,17 @@
 <template>
     <div id="app">
         <div class="animation-style" ref="animation"></div>
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
     import lottie from 'lottie-web';
-    import Trophy from './JSON/data-1.json'
+    import Trophy from './JSON/13667-new-year-2020.json'
 
     export default {
         name: 'app',
         components: {
-            HelloWorld
+
         },
         mounted() {
             lottie.loadAnimation({
@@ -29,12 +27,5 @@
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+
 </style>
